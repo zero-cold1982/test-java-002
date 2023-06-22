50bdca93f4f0af8bf9054f96f91e78868d1084a6
Ankesh Jha <ajha@davosenergy.com>
2023-06-22 15:06:01 +0530

    fix commit

--- src/main/java/com/example/api/model/Customers.java
+++ src/main/java/com/example/api/model/Customers.java
@@ -3,17 +3,61 @@ package com.example.api.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "Customers")
+@Table(name = "customers")
 public class Customers {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "Name")
-    private String Name;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "Country")
-    private String Country;
+    @Column(name = "country")
+    private String country;
 
+    
+
+    // Getter
+    public String getName() {
+      return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+      this.name = newName;
+    }
+    
+ // Getter
+    public String getCountry() {
+      return country;
+    }
+
+    // Setter
+    public void setCountry(String country) {
+      this.country = country;
+    }
+    
+ // Getter
+    public int getId() {
+      return id;
+    }
+
+    // Setter
+    public void setId(int id) {
+      this.id = id;
+    }
+
+	public Customers(int id, String name, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.country = country;
+	}
+	
+	public Customers() {
+		
+	}
+    
+    
     // Constructors, getters, and setters
 }
